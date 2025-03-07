ac060361060863dc2d8646bab8975adcad291654
Conseiga <abeltounkara1999@outlook.fr>
2025-03-07 19:17:04 +0000

    Setting up for MVVM implimentation

---
+++ ViewModels/Predicate.cs
@@ -0,0 +1,6 @@
+﻿namespace ADD_DABOMPA.ViewModels
+{
+    internal class Predicate<T1, T2>
+    {
+    }
+}
---
+++ ViewModels/ViewModelsCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ADD_DABOMPA.ViewModels
+{
+    public class ViewModelsCommand : ICommand
+    {
+        private readonly Action<object?> _executeAction;
+        private readonly Predicate<object?> _canExecuteAction;
+        public event EventHandler? CanExecuteChanged;
+
+        public ViewModelsCommand(Action<object?> executeAction, Predicate<object?> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public ViewModelsCommand(Action<object?> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecuteAction?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
